7c534750b31b9f235c31dfa0d1b0edfdbe01a131
ElyonKyla <taniveiga@gmail.com>
2023-06-04 02:46:18 +0200

    carpeta js y js principales

---
+++ js/butacas.js
@@ -0,0 +1,4 @@
+const BotonPasarelaPago =  document.querySelector("#BotonPasarelaPago");
+BotonPasarelaPago.addEventListener("click",function(){
+    console.log("Vamos a pagar las entradas");
+})
---
+++ js/entradas.js
@@ -0,0 +1,4 @@
+const botonButacas =  document.querySelector("#botonButacas");
+botonButacas.addEventListener("click",function(){
+    console.log("Vamos a seleccionar las butacas");
+})
