9dff1f284407ecd2d34116ca782ea4908ba34485
unknown <taniveiga@gmail.com>
2023-06-07 12:20:20 +0200

    añadido contador a botones y arreglo de hover

--- js/entradas.js
+++ js/entradas.js
@@ -1,3 +1,35 @@
+
+/*Carga de valores a 0 en las entradas*/
+let entradaFam = document.createElement("P");
+entradaFam.textContent=0;
+let divFamilia = document.querySelector(".entrada-familia");
+entradaFam.classList.add("pEntradaFamilia");
+divFamilia.appendChild(entradaFam);
+
+let entradaNi = document.createElement("P");
+entradaNi.textContent=0;
+let divNi = document.querySelector(".entrada-ninho");
+divNi.appendChild(entradaNi);
+
+let entradaSenior = document.createElement("P");
+entradaSenior.textContent=0;
+let divSenior = document.querySelector(".entrada-senior");
+divSenior.appendChild(entradaSenior);
+
+let entradaAdulto = document.createElement("P");
+entradaAdulto.textContent=0;
+let divAdulto = document.querySelector(".entrada-adulto");
+divAdulto.appendChild(entradaAdulto);
+
+let entradaJoven = document.createElement("P");
+entradaJoven.textContent=0;
+let divJoven = document.querySelector(".entrada-joven");
+divJoven.appendChild(entradaJoven);
+
+/* Botoneria */
+
+
+/*boton final*/
 const botonButacas =  document.querySelector("#botonButacas");
 botonButacas.addEventListener("click",function(){
     console.log("Vamos a seleccionar las butacas");
