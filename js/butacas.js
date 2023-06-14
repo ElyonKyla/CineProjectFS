ec28e5be32e0cf36f64e46902ca5ff0aaff6b732
unknown <taniveiga@gmail.com>
2023-06-14 14:33:02 +0200

    butaca seleccionable y desseleccionable

--- js/butacas.js
+++ js/butacas.js
@@ -1,13 +1,53 @@
 
 var arrayButacas = new Array();
 
-const BotonA1 =  document.querySelector("#a1");
-BotonA1.addEventListener("click",function(){
-    let colorAsiento = document.querySelector("#a1");
-    colorAsiento.style.backgroundImage="url('/img/mini-butaca-morado.png')";
-    arrayButacas.push("a1");
-    console.log(arrayButacas);
-})
+// var clicA1=0;
+// const BotonA1 =  document.querySelector("#a1");
+//     BotonA1.addEventListener("click",function(){
+//             if(clicA1==1){
+//             let colorAsiento = document.querySelector("#a1");
+//             colorAsiento.style.backgroundImage="url('/img/mini-butaca-perry.png')";
+//             //arrayButacas.push("a1");
+            
+//         // console.log(arrayButacas);
+//             clicA1 =0;
+//             console.log(clicA1);
+//             }
+            
+//         })
+
+//     BotonA1.addEventListener("click",function(){
+//         if (clicA1==0){
+//         let colorAsiento = document.querySelector("#a1");
+//         colorAsiento.style.backgroundImage="url('/img/mini-butaca-morado.png')";
+//     // arrayButacas.push("a1");
+//          clicA1 =1;
+//          console.log(clicA1);
+//         //console.log(arrayButacas);
+//         }
+       
+//     })
+
+    var clicA1 = 0;
+    const BotonA1 = document.querySelector("#a1");
+    
+    BotonA1.addEventListener("click", function () {
+        if (clicA1 == 0) {
+            let colorAsiento = document.querySelector("#a1");
+            colorAsiento.style.backgroundImage = "url('/img/mini-butaca-morado.png')";
+            clicA1 = 1;
+        } else if (clicA1 == 1) {
+            let colorAsiento = document.querySelector("#a1");
+            colorAsiento.style.backgroundImage = "url('/img/mini-butaca-perry.png')";
+            clicA1 = 0;
+        }
+        console.log(clicA1);
+    });
+    
+    
+
+
+
 const BotonA2 =  document.querySelector("#a2");
 BotonA2.addEventListener("click",function(){
     let colorAsiento = document.querySelector("#a2");
